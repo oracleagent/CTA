80f32410755c3fd87ab684fd84bca8f87fff431c
Kyle Drake <blazehavenservers@gmail.com>
2024-05-15 23:14:46 -0700

    changes

--- setup.py
+++ setup.py
@@ -1,34 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='CryptoTradingBot',
-    version='0.1',
-    packages=find_packages(),
+    name='CTA',
+    version='1.0.0',
+    description='Crypto Trading Agent',
+    author='Your Name',
+    author_email='your.email@example.com',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     install_requires=[
-        'numpy',
-        'pandas',
-        'requests',
-        'apscheduler',
-        'dash',
-        'plotly',
-        'pymongo',
-        'flask',
-        'sqlalchemy'
+        'requests==2.25.1',
+        'pandas==1.2.4',
+        'SQLAlchemy==1.4.15',
+        'APScheduler==3.6.3',
+        'ccxt==1.42.75',
+        'pytest==6.2.4',
+        'python-dotenv==0.17.0',
+        'loguru==0.5.3',
+        'websocket-client==0.59.0',
+        'ujson==4.0.2'
     ],
     entry_points={
         'console_scripts': [
-            'run-trading-bot=src.main:main',
+            'start-bot=src.main:main',
         ],
     },
-    python_requires='>=3.6',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A sophisticated crypto trading bot capable of trading multiple cryptocurrencies.',
-    keywords='crypto trading bot',
-    url='http://example.com/YourProjectHomePage',
-    project_urls={
-        'Documentation': 'http://example.com/YourProjectDocumentation',
-        'Source': 'http://github.com/yourusername/yourproject',
-        'Tracker': 'http://github.com/yourusername/yourproject/issues',
-    }
 )
